b0c213bd2253ee76b8bd9e02c0fcc790aa3beff0
Cristian Javier Padierna Higuita <122125990+CristianPadiernaCSS@users.noreply.github.com>
2025-05-26 18:08:57 -0500

    Nuevo despliegue

--- chunk-ANHB7MOQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-XRH4F7CW.js").then(o=>o.LandingComponent)}];export{t as LANDING_ROUTES};
---
+++ chunk-I7I5TRPH.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-5GMYWMJH.js").then(o=>o.LandingComponent)}];export{t as LANDING_ROUTES};
